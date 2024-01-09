841441c5278b21ed69d01ec1c1162eb83ff916a1
erikjandelange <5802245+erikjandelange@users.noreply.github.com>
2024-01-09 12:28:21 +0100

    Update serviceprovider to publish config file

--- src/LockServiceProvider.php
+++ src/LockServiceProvider.php
@@ -8,8 +8,18 @@ use Illuminate\Support\ServiceProvider;
 
 class LockServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/lock.php',
+            'lock'
+        );
+    }
+    
     public function boot(): void
     {
-        //
+        $this->publishes([
+            __DIR__ . '/../config/lock.php' => config_path('lock.php'),
+        ], 'lock-config');
     }
 }
